7d4ee9e118f02dc3498fc0ae60c5beb1c135382e
renztp <tullaorenz@gmail.com>
2026-05-21 22:29:45 +0800

    added initial methods

---
+++ src/main/java/core/TimeKeeper.java
@@ -0,0 +1,8 @@
+package core;
+
+public class TimeKeeper {
+    public Map<Integer, double[]> processAttendance() {}
+    public double calculateHoursWorked() {}
+    public String getMonthName() {}
+    public int getLastDayOfMonth() {}
+}
