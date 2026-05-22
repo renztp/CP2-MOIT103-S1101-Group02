b21c3b4e3f2d3133824ae7732597939fd7a98960
renztp <tullaorenz@gmail.com>
2026-05-22 16:37:09 +0800

    fixed conflicts

---
+++ src/main/java/shared/CSVColumnIndex.java
@@ -0,0 +1,16 @@
+package shared;
+
+public final class CSVColumnIndex {
+    public static final int COL_EMP_ID      = 0;
+    public static final int COL_LAST_NAME   = 1;
+    public static final int COL_FIRST_NAME  = 2;
+    public static final int COL_BIRTHDAY    = 3;
+    public static final int COL_HOURLY_RATE = 18;
+
+    public static final int ATT_EMP_ID   = 0;
+    public static final int ATT_DATE     = 3;
+    public static final int ATT_TIME_IN  = 4;
+    public static final int ATT_TIME_OUT = 5;
+
+    private CSVColumnIndex() {}
+}
