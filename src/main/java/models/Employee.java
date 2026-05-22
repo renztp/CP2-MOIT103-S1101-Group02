17da2ec83232dfe8a9ce049f69a90c097885562f
aesgu <aesgu@Iameanne09>
2026-05-23 01:10:36 +0800

    Added Employee and Payslip model classes

---
+++ src/main/java/models/Employee.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author aesgu
+ */
+public class Employee {
+    
+    private int employeeNumber;
+    private String lastName;
+    private String firstName;
+    private String position;
+    private String role;
+
+    private double hourlyRate;
+    private double riceSubsidy;
+    private double phoneAllowance;
+    private double clothingAllowance;
+
+    public Employee(
+            int employeeNumber,
+            String lastName,
+            String firstName,
+            String position,
+            String role,
+            double hourlyRate,
+            double riceSubsidy,
+            double phoneAllowance,
+            double clothingAllowance
+    ) {
+
+        this.employeeNumber = employeeNumber;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.position = position;
+        this.role = role;
+
+        this.hourlyRate = hourlyRate;
+        this.riceSubsidy = riceSubsidy;
+        this.phoneAllowance = phoneAllowance;
+        this.clothingAllowance = clothingAllowance;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public double getRiceSubsidy() {
+        return riceSubsidy;
+    }
+
+    public double getPhoneAllowance() {
+        return phoneAllowance;
+    }
+
+    public double getClothingAllowance() {
+        return clothingAllowance;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+}
